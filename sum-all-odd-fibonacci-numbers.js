d60374b774b04515554abebf727e15157fef0c5a
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-03 23:18:41 +0430

    solved

--- sum-all-odd-fibonacci-numbers.js
+++ sum-all-odd-fibonacci-numbers.js
@@ -1,3 +1,27 @@
 module.exports = function (num) {
-  // write your code here
+  let fibonachi = [1, 1];
+  let count = 0;
+  let nextFibo;
+  let result = 0;
+
+  if (num === 1) {
+    return 1;
+  }
+  if(num===2){
+    return2;
+  }
+
+  while (num > count) {
+    nextFibo = fibonachi[count] + fibonachi[count + 1];
+
+    if (nextFibo <= num) {
+      fibonachi.push(nextFibo);
+      if (nextFibo % 2 != 0) {
+        result += nextFibo;
+      }
+    }
+
+    count++;
+  }
+  return result;
 }
